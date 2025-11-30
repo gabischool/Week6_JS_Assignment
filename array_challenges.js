c5daac2781bca2c9142853c38faed7aa18fd8048
Abdikadir M Ali <adomow5@hotmail.com>
2025-11-30 03:03:19 +0100

    finished

--- array_challenges.js
+++ array_challenges.js
@@ -46,7 +46,25 @@ const movies = [
   
   // ✍️ Solve it here ✍️
   
-  
+  const movies = [
+    { title: "Fast & Furious 10", genre: "Action", rating: 7.5, rented: true },
+    { title: "The Notebook", genre: "Drama", rating: 8.0, rented: false },
+    { title: "Spider-Man: No Way Home", genre: "Action", rating: 8.7, rented: true },
+    { title: "Superbad", genre: "Comedy", rating: 7.0, rented: false },
+    { title: "The Dark Knight", genre: "Action", rating: 9.0, rented: true },
+    { title: "The Intern", genre: "Comedy", rating: 7.4, rented: false }
+  ];
+
+  const movieinfo = movies.map(movie => {
+
+    return '${movie.title} rating: ${movie.rating}10'
+
+  })
+
+console.log (movieinfo)
+
+
+
   /*
   Task 2: Find Highly Rated Movies 🌟 (`.filter`)
   
@@ -67,5 +85,13 @@ const movies = [
   
   // ✍️ Solve it here ✍️
 
+const movies = [
 
- 
+  { title: "The Notebook", genre: "Drama", rating: 8.0, rented: false },
+  { title: "Spider-Man: No Way Home", genre: "Action", rating: 8.7, rented: true },
+   { title: "The Dark Knight", genre: "Action", rating: 9.0, rented: true } 
+    ]
+
+ const highlyRatedMovie = movies.filter(movie => movie.rating >=8.0  ); 
+
+ console.log(highlyRatedMovie)
